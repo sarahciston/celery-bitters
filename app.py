eb967c03aad42d91314ff3ce657e727268701aec
Sarah Ciston <ciston@usc.edu>
2022-04-03 12:56:57 -0700

    database connection second attempt

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import render_template
 
 from pymongo import MongoClient
 
-# DB_URL = $SCALINGO_POSTGRESQL_URL
+MONGO_URL = "mongodb://celery-bitters-8640:8P6lfFk5L6v-V-uqYT73@ec50dbe1-ac81-45a6-84fa-9ce2354aae20.celery-bitters-8640.mongo.a.osc-fr1.scalingo-dbs.com:32787/celery-bitters-8640?replicaSet=celery-bitters-8640-rs0&ssl=true"
 
 client = MongoClient(MONGO_URL)
 
 db = client.test
 
 print(db.name) 
+print("got to here")
 
 app = Flask(__name__)
 
