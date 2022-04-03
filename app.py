ad363f2cb9339e783d190a7b6a3a6e97ffa33a39
Sarah Ciston <ciston@usc.edu>
2022-04-03 12:57:13 -0700

    typo

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import render_template
 
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb://celery-bitters-8640:8P6lfFk5L6v-V-uqYT73@ec50dbe1-ac81-45a6-84fa-9ce2354aae20.celery-bitters-8640.mongo.a.osc-fr1.scalingo-dbs.com:32787/celery-bitters-8640?replicaSet=celery-bitters-8640-rs0&ssl=true"
-
 client = MongoClient(MONGO_URL)
 
 db = client.test
