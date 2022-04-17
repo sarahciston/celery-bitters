5ee4505d7bb0a369f66a6f990c30be10e79cc474
Sarah Ciston <ciston@usc.edu>
2022-04-17 14:13:02 -0700

    testing real time voice clone as server and worker

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ from flask import send_file
 
 import scal_task
 
-# import argparse
-# import io
-# import json
-# import sys
-# from pathlib import Path
-# from typing import Union
-
-# from TTS.config import load_config
-# from TTS.utils.manage import ModelManager
-# from TTS.utils.synthesizer import Synthesizer
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -33,32 +22,12 @@ def test():
     result.wait()
     return render_template('index.html', celery=result)
 
-## SYNTHESIZER COQUI
-
-# @app.route("/synth")
-# def synth():
-#     return render_template(
-#         "synth.html",
-#         show_details=args.show_details,
-#         use_multi_speaker=use_multi_speaker,
-#         speaker_ids=speaker_manager.speaker_ids if speaker_manager is not None else None,
-#         use_gst=use_gst,
-#     )
-
-# @app.route("/api/tts", methods=["GET"])
-# def tts():
-#     text = request.args.get("text")
-#     speaker_idx = request.args.get("speaker_id", "")
-#     style_wav = request.args.get("style_wav", "")
-#     style_wav = style_wav_uri_to_dict(style_wav)
-#     print(" > Model input: {}".format(text))
-#     print(" > Speaker Idx: {}".format(speaker_idx))
-#     wavs = synthesizer.tts(text, speaker_name=speaker_idx, style_wav=style_wav)
-#     out = io.BytesIO()
-#     synthesizer.save_wav(wavs, out)
-#     return send_file(out, mimetype="audio/wav")
-
-## END COQUI
+@app.route("/clone") 
+def clone():
+    voice = request.args.get('voice')
+    result = scal_task.clone.delay(voice)
+    result.wait()
+    return render_template('clone.html', celery=result)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
